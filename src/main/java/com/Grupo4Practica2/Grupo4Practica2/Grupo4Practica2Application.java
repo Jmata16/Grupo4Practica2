0d6e6894dda297bde12b0ce381facd8013941171
Jose Daniel Mata <105888177+Jmata16@users.noreply.github.com>
2023-02-02 16:32:49 -0600

    Primer commit
    se creo el archivo

---
+++ src/main/java/com/Grupo4Practica2/Grupo4Practica2/Grupo4Practica2Application.java
@@ -0,0 +1,13 @@
+package com.Grupo4Practica2.Grupo4Practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4Practica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4Practica2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo4Practica2/Grupo4Practica2/Grupo4Practica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo4Practica2.Grupo4Practica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4Practica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
